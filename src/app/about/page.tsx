e2dd0bc92940daead9545db899b3df1274480de4
nick-sopra-steria <nicola.muneratto@soprasteria.com>
2024-10-02 18:28:47 +0200

    ---

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ const About = () => {
     return (
       <div>
         <h1>Om denne siden</h1>
-        <p>Dette er litt dyptgpende info om "om"-siden ...</p>
+        <p>Dette er litt dyptgpende info om-siden ...</p>
       </div>
     );
   };
